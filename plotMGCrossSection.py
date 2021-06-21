c91a89e7b8c01762e24f48ea1313387654d62995
Samuel Dezso Dysch <sdysch94@gmail.com>
2021-06-21 16:27:18 +0100

    New cross sections

--- plotMGCrossSection.py
+++ plotMGCrossSection.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 masses = [100, 200, 300, 400, 500, 600, 700, 800]
 
 # in pb
-crossSections_schan_Z    = [0.6524, 0.2167, 0.1149, 0.08092, 0.06834, 0.06076, 0.05154, 0.03723]
-crossSectionsUnc_schan_Z =  [0.0013, 0.00059, 0.00025, 0.00018,0.00015, 0.00014, 0.00015, 6.4E-05]
+#crossSections_schan_Z    = [0.6524, 0.2167, 0.1149, 0.08092, 0.06834, 0.06076, 0.05154, 0.03723]
+#crossSectionsUnc_schan_Z =  [0.0013, 0.00059, 0.00025, 0.00018,0.00015, 0.00014, 0.00015, 6.4E-05]
 
+crossSections_schan_Z    = [0.6533, 0.2163, 0.1147, 0.08086, 0.06821, 0.06073, 0.05151, 0.03716]
 crossSections_schan_W = [0.001246, 0.001959, 0.002191, 0.002109, 0.001876, 0.001602, 0.001329, 0.001079]
 
 if (len(crossSections_schan_Z) != len(masses)):
@@ -21,7 +22,8 @@ if (len(crossSections_schan_W) != len(masses)):
 # plot!
 fig, axis = plt.subplots(figsize = (20, 8))
 #plt.plot(masses, crossSections_schan, c = "b", label="s channel", linestyle = "", marker = 'o')
-plt.errorbar(masses, crossSections_schan_Z, yerr = crossSectionsUnc_schan_Z, c = "b", label="s channel, $Z/\gamma^*$", linestyle = "", marker = 'o')
+#plt.errorbar(masses, crossSections_schan_Z, yerr = crossSectionsUnc_schan_Z, c = "b", label="s channel, $Z/\gamma^*$", linestyle = "", marker = 'o')
+plt.plot(masses, crossSections_schan_Z, c = "b", label="s channel, $Z/\gamma^*$", linestyle = "", marker = 'o')
 plt.plot(masses, crossSections_schan_W, c = "r", label="s channel, $W^{\pm}$", linestyle = "", marker = 'o')
 
 plt.yscale('log')
